86b225c05cd1ab1b4d8a9b23aedcdec144e6d9c8
pierreobeau <beauliver@gmail.com>
2022-09-28 14:45:55 +0200

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
 
-const OWM_API_KEY = "13da5f1c748ccce942111344007f5cdb";
+const OWM_API_KEY = process.env.OWM_API_KEY;
 
 router.get("/movies", (req, res) => {
   fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
